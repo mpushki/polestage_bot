4b7389fc8fa5019dec8cca35a95ae4f971cbccce
Maria Pushkina <maria.pushkina@intel.com>
2022-09-22 20:01:57 +0300

    Add new info

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 import json
+import datetime
 
 
 bot = telebot.TeleBot('5666693511:AAE9GxOtjStnkyIJZ4RCG9B8ohCGm3RTg2E')
@@ -10,26 +11,62 @@ with open('trainers.json',"r", encoding="utf-8") as file:
     data = json.load( file)
 
 
+timetable = ""
+with open('timetable.json',"r", encoding="utf-8") as file:
+    timetable = json.load( file)
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     # bot.reply_to(message, "Добро пожаловать")
-    bot.send_message(message.chat.id, "Добро пожаловать")
-    
+    bot.send_message(message.chat.id, "Добро пожаловать в PoleStage")
+
 
 @bot.message_handler(commands=['day'])
 def get_text_messages(message):
-    bot.send_message(message.chat.id, "Рассписание на сегодня")
+    now = datetime.datetime.now()
+    print(now)
+    today_timetable = timetable[now.strftime("%A")]
+    bot.send_message(message.chat.id, f"Рассписание на сегодня {now.strftime('%d.%m')}")
+    text = '\n'.join(today_timetable)
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=['week'])
+def get_text_messages(message):
+    bot.send_message(message.chat.id, "Рассписание на неделю")
+    text = ""
+    for day, time_of_day in timetable.items():
+        text += f"{day}\n" + "\n".join(time_of_day) + "\n\n"
+    bot.send_message(message.chat.id, text)
+
 
 
 @bot.message_handler(commands=['trainers'])
 def get_text_messages(message):
     keyboard = telebot.types.InlineKeyboardMarkup(); #наша клавиатура
-    for trainer, trainer_data in data.items():
-        print(trainer)
-        # trainer_data = data[trainer]
-        key = telebot.types.InlineKeyboardButton(text=trainer_data["name"], callback_data=trainer); #кнопка «Да»
-        keyboard.add(key); #добавляем кнопку в клавиатуру
+    keys = []
+    jjj = iter(data.items())
+    for trainer, trainer_data in jjj:
+        fff = telebot.types.InlineKeyboardButton(text=trainer_data["name"], callback_data=trainer)
+        fdfdfd = next(jjj, None)
+        if fdfdfd:
+            trainer1, trainer_data1 = fdfdfd
+            print(trainer1, trainer_data1)
+            kk = telebot.types.InlineKeyboardButton(text=trainer_data1["name"], callback_data=trainer1)
+            keyboard.row(fff, kk)
+        else:
+            keyboard.row(fff)
+        # index = list(data.keys()).index(trainer)
+        # print(index)
+        # if index % 2 == 1:
+        #     keyboard.row(*keys)
+        #     keys = []
+        # key = telebot.types.InlineKeyboardButton(text=trainer_data["name"], callback_data=trainer)
+        # keys.append(key)
+        # print(keys)
+
+        # keyboard.add(key); #добавляем кнопку в клавиатуру
 
     bot.send_message(message.chat.id, "Список:", reply_markup=keyboard)
 
@@ -39,7 +76,7 @@ def callback_worker(call):
         trainer_data = data[call.data]
         print(trainer_data)
         bot.send_photo(call.message.chat.id, photo=open(f'assets/{trainer_data["image"]}.jpg', 'rb'))
-        bot.send_message(call.message.chat.id, trainer_data['name'] + "," + trainer_data['inst'] + "," + trainer_data['types'])
+        bot.send_message(call.message.chat.id, trainer_data['name'] + ", " + trainer_data['inst'] + ", " + trainer_data['types'])
         
 
 
@@ -49,7 +86,7 @@ def main() -> None:
         commands=[
             telebot.types.BotCommand("week", "Рассписание на неделю"),
             telebot.types.BotCommand("day", "Рассписание на сегодня"),
-            telebot.types.BotCommand("trainers", "Узнать о наших трененрах")
+            telebot.types.BotCommand("trainers", "Узнать о наших тренерах")
         ],
         # scope=telebot.types.BotCommandScopeChat(12345678)  # use for personal command for users
         # scope=telebot.types.BotCommandScopeAllPrivateChats()  # use for all private chats
